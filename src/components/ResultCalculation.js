014f7753f253b6bdf8e9e9bb5d250d2b485befdd
carlosalberto05 <carlos_alberto_lira@hotmail.com>
2020-06-10 15:27:45 -0500

    Se termino de crear el componente de los resultados

--- src/components/ResultCalculation.js
+++ src/components/ResultCalculation.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
 
 export default function ResultCalculation(props) {
   const {cantidad, interest, months, total, errorMessage} = props;
   return (
     <View style={styles.content}>
-      {total && <Text>Result total</Text>}
+      {total && (
+        <ScrollView style={styles.boxResult}>
+          <Text style={styles.title}>RESUMEN</Text>
+          <DataResult title="Cantidad prestada" value={`$${cantidad} MXN`} />
+          <DataResult title="Interes:" value={`${interest}%`} />
+          <DataResult title="Plazos:" value={`${months}`} meses />
+          <DataResult title="Pago mensual" value={`${total.monthlyFee} MXN`} />
+          <DataResult
+            title="Total a pagar"
+            value={`${total.totalPayable} MXN`}
+          />
+        </ScrollView>
+      )}
       <View>
         <Text style={styles.error}>{errorMessage}</Text>
       </View>
@@ -13,10 +25,34 @@ export default function ResultCalculation(props) {
   );
 }
 
+function DataResult(props) {
+  const {title, value} = props;
+
+  return (
+    <View style={styles.value}>
+      <Text>{title}</Text>
+      <Text>{value}</Text>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   content: {
-    marginTop: 100,
     marginHorizontal: 40,
+  },
+  boxResult: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  value: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
   },
   error: {
     textAlign: 'center',
